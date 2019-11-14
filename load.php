705e7682505f507c0f715d94bca40f080c3f3235
Alisson Lauffer <alissonvitortc@gmail.com>
2019-11-14 12:32:05 -0300

    Use http redirects rather than meta tags

--- load.php
+++ load.php
@@ -3,9 +3,7 @@
   $aff = $_REQUEST["q"];
   if(isset($urls[$aff])) {
     $url = $urls[$aff];
-    echo 'Redirecionando...';
-    echo "<meta http-equiv='refresh' content='0; url={$url}' />";
-    echo " Clique <a href='{$url}'>aqui</a> caso você não seja redirecionado automaticamente.";
+    header("Location: ".$url);
   }
   else {
     exit("URL inválida.");
