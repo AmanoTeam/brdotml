b77e80d6775b5ea6e6409197e7cda5b927055a28
Alisson Lauffer <alissonvitortc@gmail.com>
2019-11-14 12:07:30 -0300

    Reindent php files to 4 spaces

--- api/encurtar_url/index.php
+++ api/encurtar_url/index.php
@@ -1,28 +1,28 @@
 <?php
-	header('Content-type: application/json');
-	if (isset($_REQUEST['url'])) {
-		if ($_REQUEST['url'] == '') {
-			echo json_encode(['ok' => false, 'description' => "A URL inserida não pode estar vazia."]);
-		}
-		else {
-			if (preg_match('/^[a-zA-Z]*:\/\/.*$/', $_REQUEST['url'])) {
-				$url = $_REQUEST['url'];
-			}
-			else {
-				$url = 'http://'.$_REQUEST['url'];
-			}
-			if (preg_match('/^https?:\/\/(www\.)?(xn--f77h6a|🇧🇷)\.ml/', $url)) {
-				echo json_encode(['ok' => false, 'description' => "A URL não pode ter o domínio de nosso site."]);
-			}
-			else {
-				$path = md5($url);
-				$otherpath = substr_replace($path, '', 5, -1);
-				$urls = json_decode(file_get_contents('../../urls.json'), true);
-				$urls[$otherpath] = $url;
-				file_put_contents('../../urls.json', json_encode($urls));
-				echo json_encode(['ok' => true, 'link' => "https://🇧🇷.ml/$otherpath", 'id' => "$otherpath"]);
-			}
-		}
-	} else {
-		echo json_encode(['ok' => false, 'description' => "Está faltando argumentos. Veja https://🇧🇷.ml/docs#encurtar_url"]);
-	}
+    header('Content-type: application/json');
+    if (isset($_REQUEST['url'])) {
+        if ($_REQUEST['url'] == '') {
+            echo json_encode(['ok' => false, 'description' => "A URL inserida não pode estar vazia."]);
+        }
+        else {
+            if (preg_match('/^[a-zA-Z]*:\/\/.*$/', $_REQUEST['url'])) {
+                $url = $_REQUEST['url'];
+            }
+            else {
+                $url = 'http://'.$_REQUEST['url'];
+            }
+            if (preg_match('/^https?:\/\/(www\.)?(xn--f77h6a|🇧🇷)\.ml/', $url)) {
+                echo json_encode(['ok' => false, 'description' => "A URL não pode ter o domínio de nosso site."]);
+            }
+            else {
+                $path = md5($url);
+                $otherpath = substr_replace($path, '', 5, -1);
+                $urls = json_decode(file_get_contents('../../urls.json'), true);
+                $urls[$otherpath] = $url;
+                file_put_contents('../../urls.json', json_encode($urls));
+                echo json_encode(['ok' => true, 'link' => "https://🇧🇷.ml/$otherpath", 'id' => "$otherpath"]);
+            }
+        }
+    } else {
+        echo json_encode(['ok' => false, 'description' => "Está faltando argumentos. Veja https://🇧🇷.ml/docs#encurtar_url"]);
+    }
--- api/obter_url/index.php
+++ api/obter_url/index.php
@@ -1,18 +1,18 @@
 <?php
-  header('Content-type: application/json');
-  $urls = json_decode(file_get_contents('../../urls.json'), true);
-  if (isset($_REQUEST["id"])) {
-    $aff = $_REQUEST["id"];
-    if(isset($urls[$aff])) {
-      $url = $urls[$aff];
-      echo json_encode(['ok' => true, 'url' => $url]);
+    header('Content-type: application/json');
+    $urls = json_decode(file_get_contents('../../urls.json'), true);
+    if (isset($_REQUEST["id"])) {
+        $aff = $_REQUEST["id"];
+        if(isset($urls[$aff])) {
+            $url = $urls[$aff];
+            echo json_encode(['ok' => true, 'url' => $url]);
+        }
+
+        else {
+            echo json_encode(['ok' => false, 'description' => 'URL não encontrada.']);
+        }
     }
 
     else {
-      echo json_encode(['ok' => false, 'description' => 'URL não encontrada.']);
+        echo json_encode(['ok' => false, 'description' => "Está faltando o argumento 'id'. Veja https://🇧🇷.ml/docs"]);
     }
-  }
-
-  else {
-    echo json_encode(['ok' => false, 'description' => "Está faltando o argumento 'id'. Veja https://🇧🇷.ml/docs"]);
-  }
